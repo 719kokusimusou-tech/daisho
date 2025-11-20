19f5b5bc53808e5b987d728d794914e2d720e477
719kokusimusou-tech <719kokusimusou@gmail.com>
2025-11-20 14:09:55 +0900

    1kai

---
+++ daisho-day3/assets/js/inventory.js
@@ -0,0 +1,5 @@
+const items=[
+    {id:"D001",name:"オレンジジュース",price:150,stock:20,minStock:5},
+    {id:"D002",name:"コーラ",price:150,stock:12,minStock:5},
+    {id:"D001",name:"お茶",price:130,stock:30,minStock:10}
+];

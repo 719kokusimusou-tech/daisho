4544f00562022f183625233da60638e63307b1d2
719kokusimusou-tech <719kokusimusou@gmail.com>
2025-11-20 14:53:09 +0900

    ver5

--- daisho-day3/assets/js/inventory.js
+++ daisho-day3/assets/js/inventory.js
@@ -1,7 +1,7 @@
 const items=[
     {id:"D001",name:"オレンジジュース",price:150,stock:20,minStock:5},
     {id:"D002",name:"コーラ",price:150,stock:12,minStock:5},
-    {id:"D001",name:"お茶",price:130,stock:30,minStock:10}
+    {id:"D001",name:"お茶",price:120,stock:30,minStock:10}
 ];
 const tbody=document.getElementById("item-list");
 
